c6aa2d912328f7e000aabcab756560db03f1c9cd
Quintin Shalapata <Quintin Shalapata>
2017-03-06 11:48:29 -0500

    change code to svm but not working

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -22,10 +22,11 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
+from sklearn.svm import SVC
+clf = SVC(kernel="linear")
 t0 = time()
 clf.fit(features_train, labels_train)
+
 print "training time:", round(time()-t0, 3), "s"
 
 pred = clf.predict(features_test)
