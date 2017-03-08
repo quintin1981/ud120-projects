72dd173918cafd47119c6dbabea5984c196eba08
Quintin Shalapata <Quintin Shalapata>
2017-03-08 12:52:00 -0500

    Created code for decision tree and print accuracy

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -19,9 +19,16 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+from sklearn import tree
+
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf = clf.fit(features_train, labels_train)
+
+pred = clf.predict(features_test)
 
 
-
+from sklearn.metrics import accuracy_score
+print "Accuracy: ", accuracy_score(pred, labels_test)
 #########################################################
 ### your code goes here ###
 
