eb33519bad4186decfcd9518ddf6d99b20f10286
Quintin Shalapata <Quintin Shalapata>
2017-03-08 11:34:21 -0500

    Created code to train svm in rbf and determine # emails by chris

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -20,16 +20,28 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 from sklearn.svm import SVC
-clf = SVC(kernel="linear")
+clf = SVC(kernel="rbf", C=10000.0)
 t0 = time()
-features_train = features_train[:len(features_train)/100] 
-labels_train = labels_train[:len(labels_train)/100] 
+###features_train = features_train[:len(features_train)/100] 
+###labels_train = labels_train[:len(labels_train)/100] 
 clf.fit(features_train, labels_train)
 
 print "training time:", round(time()-t0, 3), "s"
 
 pred = clf.predict(features_test)
 
+count = len(pred)
+
+i = 0
+
+chris = 0
+
+while i < count:
+    if pred[i] == 1:
+        chris = chris + 1
+        i = i + 1
+    else:
+        i = i + 1
 
     ### calculate and return the accuracy on the test data
     ### this is slightly different than the example, 
@@ -37,6 +49,10 @@ pred = clf.predict(features_test)
     ### you might need to import an sklearn module
 from sklearn.metrics import accuracy_score
 print "Accuracy: ", accuracy_score(pred, labels_test)
+print("pred[10]: ",pred[10])
+print("pred[26]: ",pred[26])
+print("pred[50]: ",pred[50])
+print("chris: ", chris)
 #########################################################
 ### your code goes here ###
 
